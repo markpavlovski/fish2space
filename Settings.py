7d7ecefdd7e860d5d866f0a8b14ecf69e5a2b3bc
Mark Pavlovski <markpavlovski@gmail.com>
2018-02-08 18:37:18 -0800

    added Settings

---
+++ Settings.py
@@ -0,0 +1,28 @@
+import os
+
+class Settings:
+
+    # Game
+    SCREEN_SIZE = (1080,800)
+    BACKGROUND_COLOR = (6,5,4)
+    FPS = 40
+
+    # Sprites
+    SPRITE-BACKGROUND = os.path.join("assets", "sprites","background.png")
+
+
+    # Scenes
+    SCENE-GAME = 0
+    SCENE-GAME-OVER = 1
+    SCENE-HIGHSCORE = 2
+    SCENE-MENU = 3
+
+
+
+
+
+
+
+
+
+    pass
